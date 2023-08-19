b0f6fecf80b315b9261e552963a4aec1e042ffaa
viiitto <vittoria.ceccatelli@gmail.com>
2023-08-19 14:43:31 +0200

    ninth attempt

--- photo_website/subcategory/map.js
+++ photo_website/subcategory/map.js
@@ -9,6 +9,16 @@ function initMap() {
   const location8 = { lat: 23.1277667, lng: 57.3086893};
   const location9 = { lat: 1.355741, lng: 103.819857};
   const location10 = { lat: 1.290291, lng: 103.851378};
+  const location11 = { lat: -8.486213839770638, lng: 119.88061778014243};
+  const location12 = { lat: -8.5891905, lng: 119.4624119};
+  const location13 = { lat: 20.95330810546875, lng: 107.05642700195312};
+  const location14 = { lat: 22.3359668, lng: 103.8426575};
+  const location15 = { lat: 20.5723171, lng: 102.6090663};
+  const location16 = { lat: 11.568271, lng: 104.9224426};
+  const location17 = { lat: 10.4021539, lng: 104.2650618};
+  const location18 = { lat: 10.6314754, lng: 104.132637};
+  const location19 = { lat: 19.8887438, lng: 102.135898}; 
+  const location20 = { lat: 13.7524938, lng: 100.4935089};
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 3,
     center: location8,
@@ -51,6 +61,46 @@ function initMap() {
   });
   const marker10 = new google.maps.Marker({
     position: location10,
+    map: map,
+  });
+  const marker11 = new google.maps.Marker({
+    position: location11,
+    map: map,
+  });
+  const marker12 = new google.maps.Marker({
+    position: location12,
+    map: map,
+  });
+  const marker13 = new google.maps.Marker({
+    position: location13,
+    map: map,
+  });
+  const marker14 = new google.maps.Marker({
+    position: location14,
+    map: map,
+  });
+  const marker15 = new google.maps.Marker({
+    position: location15,
+    map: map,
+  });
+  const marker16 = new google.maps.Marker({
+    position: location16,
+    map: map,
+  });
+  const marker17 = new google.maps.Marker({
+    position: location17,
+    map: map,
+  });
+  const marker18 = new google.maps.Marker({
+    position: location18,
+    map: map,
+  });
+  const marker19 = new google.maps.Marker({
+    position: location19,
+    map: map,
+  });
+  const marker20 = new google.maps.Marker({
+    position: location20,
     map: map,
   });
 }
